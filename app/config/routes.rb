237454d7160d97a3fe47be96d89cef4e4902117d
werrnnnwerrrnnnnnn <swbt.tatiya@gmail.com>
2026-01-22 22:55:38 +0700

    feat: add `home` controller with index page

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "home#index"
 end
